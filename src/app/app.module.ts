b815ad79483a142dce3498ee8c29c4c25b82e865
kezarmader <kezar.mader@gmail.com>
2018-01-21 15:04:01 -0600

    Initial Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,32 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ContactProvider } from '../providers/contact/contact';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ContactProvider } from '../../providers/contact/contact';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public jamaats = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private contactProvider: ContactProvider) {
 
   }
 
+  ngOnInit(): void {
+
+  }
+
+  public filterItems(event) {
+    let val = event.target.value;
+    
+    this.contactProvider.getContacts().subscribe(data => {
+      this.jamaats = data.filter(e => val && val.trim() != '' && e.jamaat.toLowerCase().indexOf(val.trim().toLowerCase()) != -1)
+    });
+  }
 }
